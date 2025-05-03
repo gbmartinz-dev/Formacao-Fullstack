6decca1815e1ae3d5c46cc453c7d7018d0c8861f
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-05-02 21:12:43 -0300

    Aprendendo sobre estruturas básicas do HTML

---
+++ javascript/manipulando-variaveis/script.js
@@ -0,0 +1,9 @@
+// Manipulando variáveis
+
+var nome = prompt("Qual é o seu nome?");
+var sobrenome = prompt("Qual é o seu sobrenome?");
+var idade = prompt("Qual é a sua idade?");
+var cidade = prompt("Qual é a sua cidade?");
+var estado = prompt("Qual é o estado em que você mora?");
+
+document.writeln("<h1> Olá " + nome + " " + sobrenome + ", você tem " + idade + " anos e mora em " + cidade + ", " + estado + ". </h1>");
