fbf4fda86f2965d2979533300842a784f37ae3e4
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-06 23:03:09 -0300

    Começando com o Java

--- BACKEND/JAVA/OlaMundo.java
+++ BACKEND/JAVA/OlaMundo.java
@@ -1,6 +1,6 @@
 public class OlaMundo {
   public static void main(String[] args) {
     System.out.println("Olá, Mundo!");
-    System.out.println("Bem-vindo ao Java!");;
+    System.out.println("Bem-vindo ao Java!");
   }
 }
