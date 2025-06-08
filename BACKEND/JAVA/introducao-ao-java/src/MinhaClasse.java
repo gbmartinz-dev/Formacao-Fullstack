6fec90d10033589a7db745eb93bdf95beae8ab8a
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-08 14:28:19 -0300

    Entendendo sobre variáveis e como declarar

--- BACKEND/JAVA/introducao-ao-java/src/MinhaClasse.java
+++ BACKEND/JAVA/introducao-ao-java/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
   public static void main(String[] args) {
-    System.out.println("Olá, Mundo! Seja Bem-vindo ao Java!");
+   String primeiroNome = "Gabriel";
+   String segundoNome = "Pereira";
+
+   String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+   System.out.println(nomeCompleto); 
+  }
+
+  public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
   }
 }
