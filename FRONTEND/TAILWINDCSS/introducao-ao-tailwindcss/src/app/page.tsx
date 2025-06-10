680678d2c84e724ee10c20e37d3f49576d15f0af
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-09 22:06:36 -0300

    Editando as primeiras linhas de código com tailwindcss

--- FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
+++ FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Olá NextJS</h1>
+      <h1 className="text-3xl font-bold underline bg-green-700 p-3">Olá NextJS</h1>
     </div>
   );
 }
