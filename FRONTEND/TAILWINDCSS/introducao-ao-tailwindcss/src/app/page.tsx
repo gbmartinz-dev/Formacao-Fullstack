a2633014e3de78d105a3b9ad7f136b3c6466970b
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-12 20:36:04 -0300

    Manipulando textos no tailwindcss

--- FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
+++ FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
@@ -2,6 +2,7 @@ const Page = () => {
   return (
     <div className="w-screen h-screen">
       <div className="bg-blue-600 w-36 h-36 p-2 my-8 mx-16">Olá Mundo!</div>
+      <div className="text-sm bg-blue-600 font-serif text-center">Algum texto qualquer</div>
     </div>
   );
 }
