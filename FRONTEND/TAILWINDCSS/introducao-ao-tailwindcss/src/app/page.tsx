3480f5e37e69483a0321f307da2c4afabbb1dcb5
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-12 20:56:10 -0300

    Fazendo um botão com o tailwindcss

--- FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
+++ FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
@@ -1,9 +1,8 @@
 const Page = () => {
   return (
-    <div className="w-screen h-screen">
-      <div className="bg-blue-600 w-36 h-36 p-2 my-8 mx-16">Olá Mundo!</div>
-      <div className="text-sm bg-blue-600 font-serif text-center border-2 border-dotted">Algum texto qualquer</div>
-      <input className="w-80 outline-2 outline-green-400" />
+    <div className="w-screen h-screen p-5">
+      <button className="bg-green-700 text-white rounded-md px-5 py-2 border-none">Clique aqui</button>
+      <button className="text-white rounded-md px-5 py-2 border border-green-700">Clique aqui</button>
     </div>
   );
 }
