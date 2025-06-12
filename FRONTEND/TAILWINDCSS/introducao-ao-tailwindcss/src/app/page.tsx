582531f436a7032a0035b33f75fce8aa9e7bb6e2
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-12 20:49:11 -0300

    Manipulando bordas no tailwindcss

--- FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
+++ FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
@@ -2,7 +2,8 @@ const Page = () => {
   return (
     <div className="w-screen h-screen">
       <div className="bg-blue-600 w-36 h-36 p-2 my-8 mx-16">Olá Mundo!</div>
-      <div className="text-sm bg-blue-600 font-serif text-center">Algum texto qualquer</div>
+      <div className="text-sm bg-blue-600 font-serif text-center border-2 border-dotted">Algum texto qualquer</div>
+      <input className="w-80 outline-2 outline-green-400" />
     </div>
   );
 }
