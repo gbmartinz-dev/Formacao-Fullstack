333d9a7f6d62b4a4bb3cf4f78bae486fa8ec8ca1
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-13 14:23:13 -0300

    exercício com tailwindcss alert

--- FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
+++ FRONTEND/TAILWINDCSS/introducao-ao-tailwindcss/src/app/page.tsx
@@ -1,8 +1,10 @@
 const Page = () => {
   return (
     <div className="w-screen h-screen p-5">
-      <button className="bg-green-700 text-white rounded-md px-5 py-2 border-none">Clique aqui</button>
-      <button className="text-white rounded-md px-5 py-2 border border-green-700">Clique aqui</button>
+      <div className="bg-green-100 border-l-4 border-green-700 p-2 text-green-800 text-center">
+        Você não tem mais acesso. 
+        <a href="" className="underline ml-1">Faça o upgrade agora.</a>
+      </div>
     </div>
   );
 }
