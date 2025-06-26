ec05535e9d0992b76ed609423647322315b6427f
gbmartinz-dev <gabrielpereiramartins49@gmail.com>
2025-06-26 13:11:05 -0300

    Estudando variáveis e operadores

---
+++ FRONTEND/JAVASCRIPT/1-variaveis-e-operadores/aula1.js
@@ -0,0 +1,11 @@
+// Temos várias variáveis como VAR, LET, CONST
+// Nos dias de hoje acostumamos a utilizar a variável LET e a constante CONST
+
+// LET pode ser alterada
+// CONST não pode ser alterada
+// EX:
+
+const meuNome = "Gabriel";
+let minhaIdade = 26;
+
+console.log(meuNome + ' ' + minhaIdade);
